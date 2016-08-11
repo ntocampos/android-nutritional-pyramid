1955c6140e48427a0386b78c2ffac5e1569f67cc
ntocampos <mscn@cin.ufpe.br>
2016-08-11 20:55:01 -0300

    Moving the logic to the fragment and adding a scroll view to it in case of vertical orientation

--- app/src/main/java/com/example/ntocampos/nutritionalpyramid/MainActivityFragment.java
+++ app/src/main/java/com/example/ntocampos/nutritionalpyramid/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.example.ntocampos.nutritionalpyramid;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,57 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button calculateButton = (Button) view.findViewById(R.id.calculateButton);
+        final EditText weightEdit = (EditText) view.findViewById(R.id.weightEditText);
+        final EditText carbsEdit = (EditText) view.findViewById(R.id.carbsEditText);
+        final EditText proteinEdit = (EditText) view.findViewById(R.id.proteinEditText);
+        final EditText fatEdit = (EditText) view.findViewById(R.id.fatEditText);
+
+        if (calculateButton == null) throw new AssertionError();
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Boolean error = false;
+
+                String weightString = weightEdit.getText().toString();
+                String carbsPercentString = carbsEdit.getText().toString();
+                String proteinPercentString = proteinEdit.getText().toString();
+                String fatPercentString = fatEdit.getText().toString();
+
+                float weight, carbsPercent, proteinPercent, fatPercent;
+
+                if (weightString.isEmpty() || carbsPercentString.isEmpty() ||
+                        proteinPercentString.isEmpty() || fatPercentString.isEmpty()) {
+                    error = true;
+                }
+
+                if (!error) {
+                    weight = Float.valueOf(weightString);
+                    carbsPercent = Float.valueOf(carbsPercentString);
+                    proteinPercent = Float.valueOf(proteinPercentString);
+                    fatPercent = Float.valueOf(fatPercentString);
+
+                    Toast.makeText(getActivity().getApplicationContext(), "Calculating..",
+                            Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getActivity().getApplicationContext(),
+                            ResultsActivity.class);
+                    intent.putExtra(MainActivity.EXTRA_WEIGHT, weight);
+                    intent.putExtra(MainActivity.EXTRA_CARBS, carbsPercent);
+                    intent.putExtra(MainActivity.EXTRA_PROTEIN, proteinPercent);
+                    intent.putExtra(MainActivity.EXTRA_FAT, fatPercent);
+                    startActivity(intent);
+                }
+                else {
+                    Snackbar.make(view, "Please, fill all the inputs before proceed...",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return view;
     }
 }
